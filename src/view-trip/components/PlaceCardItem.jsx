6a1d16a68c0ae1cb743b25a56e3ddf01dcba1b11
Keshav Dussal <ksdussal96@gmail.com>
2024-09-30 18:45:45 +0530

    globalApi file import correction

--- src/view-trip/components/PlaceCardItem.jsx
+++ src/view-trip/components/PlaceCardItem.jsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { GetPlaceDetails, PHOTO_REF_URL } from "@/service/globalApi";
+import { GetPlaceDetails, PHOTO_REF_URL } from "@/service/GlobalApi";
 import React, { useEffect, useState } from "react";
 import { FaMapLocationDot } from "react-icons/fa6";
 import { Link } from "react-router-dom";
